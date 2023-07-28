3fd56c397a4d93e80890f75002134f60b892325a
Manish Yadav <112152934+mk4176241@users.noreply.github.com>
2023-07-28 08:27:27 +0000

    data types

---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+console.log(3
+    + 3
+) // it is working fine but not a good practice, code readablity should be high.
+
+
+let name = "Manish"
+let age = 24
+let isLoggedIn = false
+
+
+/*
+primitive Data types:-
+
+number => 2 to power 53
+bigint
+string => "it is used single/double quote only"
+boolean=> true/false
+null => standalone value
+undefined => when value is not assigned in a variable
+symbol => unique 
+*/
+
+//object:-
+
+console.log(typeof null);// object data types
+console.log(typeof undefined) // undefined data types
